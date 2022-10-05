26ce330842813f5002f009b53656341a171ed65f
areum <jangar6621@gmail.com>
2022-10-05 21:03:22 +0900

    TEST!!

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './app/App';
+import configureStore from './redux/index';
 import '@styles';
 
-import configureStore from '../src/redux/index';
 const store = configureStore();
 
 ReactDOM.render(
